478a10cbecad6f49aaa542a9d6ca115b642599ae
Amor Zephyr <amorzephyr@gmail.com>
2026-01-27 22:54:53 +0500

    Add profile and skills sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { TextInput, Textarea, Slider, ProfilePic } from './components'
+import { TextInput, Textarea, Slider, ProfilePic,AddButton,  Heading } from './components'
 import './layout.css'
 import './styles.css'
 const Title = ({unlocked}) => {
@@ -19,11 +19,63 @@ const Header = ({unlocked}) => {
     </section>
   )
 }
+
+const Profile = ({unlocked}) => {
+  const myProfile = 'Frontend developer with a focus on React, building clean and responsive user interfaces. I have working knowledge of backend fundamentals and a strong foundation in C++, enabling effective problem-solving and well-structured code.'
+  return (
+    <section tabIndex={1} id='profile' className='section flex-column'>
+      <Heading id='profile-heading' value={'PROFILE'}></Heading>
+      <Textarea id='profile-text' unlocked={unlocked} initialValue={myProfile} placeholder={'Something about your professional self!'}></Textarea>
+    </section>
+  )
+}
+
+const Skills = ({unlocked}) => {
+  const [skills, setSkills] = useState([
+    {id: 'html', label: 'HTML', unlocked: unlocked, initialValue: 80},
+    {id: 'css', label: 'CSS', unlocked: unlocked, initialValue: 85},
+    {id: 'js', label: 'JS', unlocked: unlocked, initialValue: 85},
+    {id: 'react', label: 'React', unlocked: unlocked, initialValue: 50},
+    {id: 'nodejs', label: 'NodeJS', unlocked: unlocked, initialValue: 40},
+    {id: 'restapi', label: 'RestAPI', unlocked: unlocked, initialValue: 60}
+  ]);
+  const newSkill = {id: crypto.randomUUID(), label: 'New Skill', unlocked: unlocked, initialValue: 50 };
+  const isExpandable = () => {
+    return skills.length < 7 ? true: false;
+  }
+  const handleAddBtn = () => {
+    if(unlocked && isExpandable()) {
+      setSkills([...skills, newSkill])
+    }
+  }
+  const handleDel = (e) => {
+    const newSkills = skills.filter(skill => `${skill.id}-del` !== e.target.id);
+    setSkills([...newSkills])
+  }
+  return (
+    <section 
+      tabIndex={2}
+      id='skills'
+      className='flex-column'
+    >
+      <Heading id='skill-heading' value='SKILLS'></Heading>
+      <div className="flex-column all-sliders">
+      {skills.map(skil => (
+        <Slider id={skil.id} key={skil.id} label={skil.label} unlocked={unlocked} initialValue={skil.initialValue} handleDel={handleDel}></Slider>
+      ))}
+      </div>
+      <AddButton id='skill-add-btn' unlocked={unlocked} expandable={isExpandable()} handleClick={handleAddBtn} style={{display: unlocked && isExpandable() ? 'block' : 'none'}}></AddButton>
+    </section>
+  )
+
+}
 const App = () => {
   const [unlocked, setUnlocked] = useState(true);
   return (
     <>
      <Header unlocked={unlocked}></Header>
+     <Profile unlocked={unlocked}></Profile>
+     <Skills unlocked={unlocked}></Skills>
     </>
   )
 }
